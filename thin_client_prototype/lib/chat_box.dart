6855465745b58e4f8c03c254b6878f4af99b91fd
JeremyEar <jeremy_ear@hotmail.com>
2024-01-22 19:53:59 -0500

    feat: Added Align widget to position text messages on their respective sides and added a colored body to them

--- thin_client_prototype/lib/chat_box.dart
+++ thin_client_prototype/lib/chat_box.dart
@@ -9,9 +9,18 @@ class ChatBox extends StatefulWidget {
 
 class _ChatBoxState extends State<ChatBox> {
   List<ChatMessage> messages = [
-    ChatMessage('sent', 'hello', 'mark', 'assets/dsdsd'),
-    ChatMessage('sent', 'hello', 'mark', 'assets/dsdsd'),
-    ChatMessage('sent', 'hello', 'James', 'assets/dsdsd')
+    ChatMessage('sent', 'hello Zak', 'Mark', 'assets/dsdsd'),
+    ChatMessage(
+        'sent',
+        'I wanted to test out that writing a super long message wouldnt ruin the display of these text messages',
+        'Mark',
+        'assets/dsdsd'),
+    ChatMessage('received', 'good day mate', 'Zak', 'assets/dsdsd'),
+    ChatMessage(
+        'received',
+        'No worries bro I was testing it out myself over here. Did you buy yo mamas christmas gift',
+        'Zak',
+        'assets/dsdsd'),
   ];
   @override
   Widget build(BuildContext context) {
@@ -54,9 +63,24 @@ class _ChatBoxState extends State<ChatBox> {
               child: ListView.builder(
                 itemCount: messages.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Text(
-                    '${messages[index].userName}: ${messages[index].message}',
-                    style: TextStyle(color: Colors.black),
+                  return Align(
+                    alignment: messages[index].tag == 'sent'
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: messages[index].tag == 'sent'
+                            ? Colors.blue
+                            : Colors.green,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        '${messages[index].userName}: ${messages[index].message}',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
                   );
                 },
               ),
