1f1656d9efff51b3d274fbc4221291006518f977
Jeremy Ear <jeremy_ear@hotmail.com>
2024-01-22 22:11:53 -0500

    feat: working chat box with sending and receiving display, still working on the name display on text messages

--- thin_client_prototype/lib/chat_box.dart
+++ thin_client_prototype/lib/chat_box.dart
@@ -12,16 +12,33 @@ class _ChatBoxState extends State<ChatBox> {
     ChatMessage('sent', 'hello Zak', 'Mark', 'assets/dsdsd'),
     ChatMessage(
         'sent',
-        'I wanted to test out that writing a super long message wouldnt ruin the display of these text messages',
+        'wanted to test out that writing a super long message wouldnt ruin the display of these text messages. Sorry for bothering you right now, even though I know your probably having fun at home with your raccoon friends you little raccoon',
         'Mark',
         'assets/dsdsd'),
     ChatMessage('received', 'good day mate', 'Zak', 'assets/dsdsd'),
     ChatMessage(
         'received',
-        'No worries bro I was testing it out myself over here. Did you buy yo mamas christmas gift',
+        'No worries bro I was testing it out myself over here. Did you buy yo mamas christmas gift? She precisely said that she wanted something for her kitchen, something expensive',
         'Zak',
         'assets/dsdsd'),
   ];
+  TextEditingController messageController = TextEditingController();
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  void scrollToBottom() {
+    scrollController.animateTo(
+      scrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 180),
+      curve: Curves.easeOut,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -61,19 +78,19 @@ class _ChatBoxState extends State<ChatBox> {
             child: Container(
               padding: EdgeInsets.all(10),
               child: ListView.builder(
+                controller: scrollController,
                 itemCount: messages.length,
                 itemBuilder: (BuildContext context, int index) {
+                  bool isSent = messages[index].tag == 'sent';
                   return Align(
-                    alignment: messages[index].tag == 'sent'
-                        ? Alignment.centerRight
-                        : Alignment.centerLeft,
+                    alignment:
+                        isSent ? Alignment.centerRight : Alignment.centerLeft,
                     child: Container(
+                      width: 250,
                       margin: EdgeInsets.symmetric(vertical: 5),
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        color: messages[index].tag == 'sent'
-                            ? Colors.blue
-                            : Colors.green,
+                        color: isSent ? Colors.blue : Colors.green,
                         borderRadius: BorderRadius.circular(8),
                       ),
                       child: Text(
@@ -92,6 +109,7 @@ class _ChatBoxState extends State<ChatBox> {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: messageController,
                     decoration: InputDecoration(
                       hintText: "Type your message...",
                       border: OutlineInputBorder(),
@@ -103,7 +121,17 @@ class _ChatBoxState extends State<ChatBox> {
                 SizedBox(width: 10),
                 ElevatedButton(
                   onPressed: () {
-                    print("hello");
+                    String message = messageController.text;
+                    if (message.isNotEmpty) {
+                      setState(() {
+                        messages.add(ChatMessage(
+                            'sent', message, 'Mark', 'assets/asdasa'));
+                      });
+                      messageController.clear();
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        scrollToBottom();
+                      });
+                    }
                   },
                   child: Text("Send"),
                 ),
