421108581148979f6a11d71fb61e43d0314dff2d
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2024-02-01 12:25:02 -0500

    fix: pipeline trigger

--- server/app/gateways/game/game.gateway.ts
+++ server/app/gateways/game/game.gateway.ts
@@ -226,6 +226,8 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect, On
         this.logger.log(`Connexion par l'utilisateur avec id : ${socket.id}`);
     }
 
+    // Test commit
+
     async handleDisconnect(@ConnectedSocket() socket: Socket) {
         this.logger.log(`Déconnexion par l'utilisateur avec id : ${socket.id}`);
         this.mapSocketWithName.delete(socket.id);
