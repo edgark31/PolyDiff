e34c9de6486adf4c209817504ab30ab1230aeda6
MarieJadeMarcil <marie-jade.marcil@polymtl.ca>
2024-02-13 14:54:19 -0500

    chatBox interface to ChatboxModel

--- mobile/lib/models/chat_message_model.dart
+++ mobile/lib/models/chat_message_model.dart
@@ -5,11 +5,30 @@ class ChatMessageModel {
   String message;
   String userName;
   String timestamp;
+  ChatMessageModel(this.tag, this.message, this.userName, this.timestamp);
 
-  ChatMessageModel({
-    required this.tag,
-    required this.message,
-    required this.userName,
-    required this.timestamp,
-  });
+  // ChatMessageModel({
+  //   required this.tag,
+  //   required this.message,
+  //   required this.userName,
+  //   required this.timestamp,
+  // })
+
+  static ChatMessageModel fromJson(Map<String, dynamic> json) {
+    return ChatMessageModel(
+      MessageTag.values.firstWhere((element) => element.name == json['tag']),
+      json['message'],
+      json['userName'],
+      json['timestamp'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'tag': tag.name,
+      'message': message,
+      'userName': userName,
+      'timestamp': timestamp,
+    };
+  }
 }
