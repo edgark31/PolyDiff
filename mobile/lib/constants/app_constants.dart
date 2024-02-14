fe0ddcc4fefbfe711688e13cf003be3d9f97be2f
MarieJadeMarcil <marie-jade.marcil@polymtl.ca>
2024-02-14 15:53:47 -0500

    added constants folder

---
+++ mobile/lib/constants/app_constants.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+const kDark = Color(0xFF000000);
+const kLight = Color(0xFFFFFFFF);
+const kGreen = Color(0xFF43A047);
+const kLightGreen = Color(0xFF388E3C);
+const kMidGreen = Color(0xFF2E7D32);
+const kDarkGreen = Color(0xFF1B5E20);
+
+double height = 812.h;
+double width = 375.w;
+
+String theId = "";
