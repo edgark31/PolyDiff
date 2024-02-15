f5b8665cd32dd8a03665f3810e6a44a27e69590e
MarieJadeMarcil <marie-jade.marcil@polymtl.ca>
2024-02-15 09:15:08 -0500

    Create app_theme_constants.dart

---
+++ mobile/lib/common/app_theme_constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+const kDark = Color(0xFF000000);
+const kLight = Color(0xFFFFFFFF);
+const kGreen = Color(0xFF43A047);
+const kLightGreen = Color(0xFF388E3C);
+const kMidGreen = Color.fromRGBO(123, 142, 5, 1);
+const kDarkGreen = Color.fromARGB(255, 4, 41, 7);
+
+const kLightOrange = Color(0xFFEF6C00);
