5cb141f5388727374a057ca7ebef426443177dfc
JeremyEar <jeremy_ear@hotmail.com>
2024-02-15 09:28:48 -0500

    feat: start of name generation service

---
+++ mobile/lib/services/name_generation_service.dart
@@ -0,0 +1,8 @@
+import '../common/random_name_lists.dart';
+
+class NameGenerationService {
+  String generatedName = '';
+  void generateName(int languageIndex, bool hasAnimalName, bool hasNumber) {
+    
+  }
+}
