13f652b66eb3bd29472686259e83d9e4be527512
MarieJadeMarcil <marie-jade.marcil@polymtl.ca>
2024-02-21 17:31:23 -0500

    fixed navigation homePage to LoginPage

--- mobile/lib/pages/home_page.dart
+++ mobile/lib/pages/home_page.dart
@@ -1,107 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screen_scaler/flutter_screen_scaler.dart';
-import 'package:get/get.dart';
 import 'package:mobile/constants/app_constants.dart';
 import 'package:mobile/pages/login_page.dart';
-import 'package:mobile/pages/registration_page.dart';
+import 'package:mobile/pages/signup_page.dart';
 import 'package:mobile/views/common/customs/app_style.dart';
 import 'package:mobile/views/common/customs/custom_btn.dart';
-import 'package:mobile/views/common/customs/height_spacer.dart';
-import 'package:mobile/views/common/customs/reusable_text.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:mobile/widgets/background_image.dart.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key});
 
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    ScreenScaler scaler = ScreenScaler()..init(context);
     double screenHeight = MediaQuery.of(context).size.height;
     double startingPoint = screenHeight * 0.5;
-
-    return Scaffold(
-      body: Container(
-        color: Color(kLightGreen.value),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Stack(
-                children: [
-                  Image.asset(
-                    "assets/images/MenuBackground.jpg",
-                    width: double.infinity,
-                    height: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
+    return Stack(
+      children: [
+        BackgroundImage(),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
                   Padding(
                     padding: EdgeInsets.only(top: startingPoint),
-                    child: Column(
-                      children: [
-                        ReusableText(
-                          text: "PolyDiff",
-                          style: appstyle(
-                              30, Color(kLightOrange.value), FontWeight.w600),
-                        ),
-                        HeightSpacer(size: 70),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CustomButton(
-                              onTap: () async {
-                                final SharedPreferences prefs =
-                                    await SharedPreferences.getInstance();
-                                await prefs.setBool('entrypoint', true);
-                                Get.to(() => const LoginPage());
-                              },
-                              text: "Connexion",
-                              width: scaler.getWidth(20),
-                              height: scaler.getHeight(10),
-                              color: Color(kLight.value),
-                              color2: Color(kMidGreen.value),
-                            ),
-                            Flexible(
-                              child: SizedBox(width: scaler.getWidth(5)),
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                Get.to(() => RegistrationPage());
-                              },
-                              child: CustomButton(
-                                text: "Inscription",
-                                width: scaler.getWidth(20),
-                                height: scaler.getHeight(10),
-                                color: Color(kMidGreen.value),
-                                color2: Color(kLight.value),
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 30),
-                        Align(
-                          alignment: Alignment.bottomLeft,
-                          child: ReusableText(
-                            text: "@ RACCOON VILLAGE ",
-                            style: appstyle(
-                              3,
-                              Color(kDarkGreen.value),
-                              FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
+                    child: Text(
+                      "PolyDiff",
+                      style: appstyle(60, kLightOrange, FontWeight.w600),
+                      textAlign: TextAlign.center,
                     ),
+                  ),
+                  SizedBox(height: 50),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomButton(
+                        text: 'C O N N E X I O N',
+                        press: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ),
+                          );
+                        },
+                        backgroundColor: kMidGreen,
+                      ),
+                      SizedBox(width: 125),
+                      CustomButton(
+                        text: "S' I N S C R I R E",
+                        press: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => SignUpPage(),
+                            ),
+                          );
+                        },
+                        backgroundColor: kLight,
+                        textColor: kMidGreen,
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
--- mobile/lib/pages/login_page.dart
+++ mobile/lib/pages/login_page.dart
@@ -1,55 +1,18 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:mobile/constants/app_constants.dart';
-import 'package:mobile/providers/login_provider.dart';
-import 'package:mobile/views/common/customs/custom_app_bar.dart';
-import 'package:mobile/widgets/bottom_nav_bar.dart';
-import 'package:provider/provider.dart';
+import 'package:mobile/widgets/background_image.dart.dart'; // Make sure the path is correct
+import 'package:mobile/widgets/connection_form.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController email = TextEditingController();
-  final TextEditingController password = TextEditingController();
-
-  @override
-  void dispose() {
-    email.dispose();
-    password.dispose();
-    super.dispose();
-  }
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<LoginNotifier>(builder: (context, loginNotifier, child) {
-      loginNotifier.getPrefs();
-      print(loginNotifier.entrypoint);
-      return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(50),
-          child: CustomAppBar(
-            text: "C O N N E X I O N",
-            child: loginNotifier.entrypoint && !loginNotifier.loggedIn
-                ? GestureDetector(
-                    onTap: () {
-                      Get.back();
-                    },
-                    child: const Icon(CupertinoIcons.arrow_left),
-                  )
-                : SizedBox.shrink(),
-          ),
-        ),
-        body: Container(
-          color: kLime,
-          child: BottomNavBar(),
-        ),
-      );
-    });
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          BackgroundImage(),
+          ConnectionForm(),
+        ],
+      ),
+    );
   }
 }
