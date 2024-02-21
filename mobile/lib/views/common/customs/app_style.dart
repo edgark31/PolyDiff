35164ae4e1b758ae9c45bfe23eb1a160c71f09eb
MarieJadeMarcil <marie-jade.marcil@polymtl.ca>
2024-02-21 17:32:37 -0500

    Update app_style.dart

--- mobile/lib/views/common/customs/app_style.dart
+++ mobile/lib/views/common/customs/app_style.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 TextStyle appstyle(double size, Color color, FontWeight fontWeight) {
   return GoogleFonts.poppins(
-      fontSize: size.sp, color: color, fontWeight: fontWeight);
+      fontSize: size, color: color, fontWeight: fontWeight);
 }
+
+const TextStyle kHeading = TextStyle(
+  fontSize: 60,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+const TextStyle kBodyText = TextStyle(
+  fontSize: 22,
+  color: Colors.white,
+);
