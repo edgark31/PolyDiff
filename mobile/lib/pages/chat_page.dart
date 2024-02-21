e9df2b904bcdd99577a279f75463282c33a14f9c
MarieJadeMarcil <marie-jade.marcil@polymtl.ca>
2024-02-21 17:32:00 -0500

    added chatPage missing file from merge

---
+++ mobile/lib/pages/chat_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/widgets/chat_box.dart';
+
+class ChatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/MenuBackground.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: ChatBox(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
