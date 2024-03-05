94dc335af5d75f9a61900727916e22c182fea852
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2024-03-05 10:36:51 -0500

    fix: remoived googlefonts

--- mobile/lib/widgets/customs/app_style.dart
+++ mobile/lib/widgets/customs/app_style.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 TextStyle appstyle(double size, Color color, FontWeight fontWeight) {
-  return GoogleFonts.poppins(
-      fontSize: size, color: color, fontWeight: fontWeight);
+  return TextStyle(fontSize: size, color: color, fontWeight: fontWeight);
+  // return GoogleFonts.poppins(
+  //     fontSize: size, color: color, fontWeight: fontWeight);
 }
 
 const TextStyle kHeading = TextStyle(
