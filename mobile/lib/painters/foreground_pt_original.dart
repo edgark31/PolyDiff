a36b8a82f998e3c390e6f243f876fc6a3f81b9f1
JeremyEar <jeremy_ear@hotmail.com>
2024-03-06 19:32:39 -0500

    feat: added background and foreground painters for original canvas

---
+++ mobile/lib/painters/foreground_pt_original.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/models/canvas_model.dart';
+import 'package:mobile/widgets/game_canvas.dart';
+
+// TODO: Make calls for validation
+class ForegroundPtOriginal extends CustomPainter {
+  final CanvasModel images;
+
+  ForegroundPtOriginal(this.images);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.scale(GameCanvas.tabletScalingRatio, GameCanvas.tabletScalingRatio);
+
+    final path = Path();
+    /*for (var coord in diffService.coordinates) {
+      path.addRect(Rect.fromPoints(
+          Offset(coord.x.toDouble(), coord.y.toDouble()),
+          Offset(coord.x + 1, coord.y + 1)));
+    }
+    canvas.clipPath(path);
+    canvas.drawImage(images.modified, Offset.zero, Paint());
+
+    if (diffService.blinkingDifference != null) {
+      canvas.drawPath(
+          diffService.blinkingDifference!, diffService.defaultBlinkingColor);
+    }*/
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
