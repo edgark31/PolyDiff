8228d90044a7bcf317a276df3900bee515c0279d
ZakariaZair <zakaria.zair@polymtl.ca>
2024-03-08 16:25:28 -0500

    feat: added correct lobbies update

--- server/app/gateways/lobby/lobby.gateway.ts
+++ server/app/gateways/lobby/lobby.gateway.ts
@@ -2,7 +2,7 @@ import { AccountManagerService } from '@app/services/account-manager/account-man
 import { LobbyEvents } from '@common/enums';
 import { Lobby } from '@common/game-interfaces';
 import { Logger } from '@nestjs/common';
-import { ConnectedSocket, OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
@@ -12,21 +12,23 @@ export class LobbyGateway implements OnGatewayConnection {
     @WebSocketServer() private server: Server;
     private readonly lobbies = new Map<string, Lobby>();
 
-    constructor(private readonly logger: Logger, private readonly accountManager: AccountManagerService) {}
+    constructor(private readonly logger: Logger, private readonly accountManager: AccountManagerService) {
+        this.lobbies = new Map<string, Lobby>();
+    }
 
     @SubscribeMessage(LobbyEvents.Create)
-    async create(@ConnectedSocket() socket: Socket, lobby: Lobby) {
+    create(@ConnectedSocket() socket: Socket, @MessageBody() lobby: Lobby) {
         lobby.lobbyId = socket.data.id;
         socket.join(lobby.lobbyId);
         this.lobbies.set(lobby.lobbyId, lobby);
-        this.update();
-        this.logger.log(`${this.accountManager.connectedUsers.get(socket.data.id)} crée un lobby`);
+        this.server.emit(LobbyEvents.UpdateLobbys, this.lobbies);
+        this.logger.log(`${this.accountManager.connectedUsers.get(socket.data.id).credentials.username} crée un lobby`);
     }
 
     @SubscribeMessage(LobbyEvents.Join)
     join(@ConnectedSocket() socket: Socket, lobbyId: string) {
         socket.join(lobbyId);
-        this.logger.log(`${this.accountManager.connectedUsers.get(socket.data.id)} rejoint le lobby ${lobbyId}`);
+        this.logger.log(`${this.accountManager.connectedUsers.get(socket.data.id).credentials.username} rejoint le lobby ${lobbyId}`);
     }
 
     @SubscribeMessage(LobbyEvents.UpdateLobbys)
