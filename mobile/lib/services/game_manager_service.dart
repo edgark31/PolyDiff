1ba67ea2f14a8c1debc176737f6708cb75f54433
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2024-03-08 16:09:25 -0500

    feat : create difference from temp difference data
    Co-authored-by: JeremyEar <JeremyEar@users.noreply.github.com>

--- mobile/lib/services/game_manager_service.dart
+++ mobile/lib/services/game_manager_service.dart
@@ -1,3 +1,22 @@
+import 'package:mobile/constants/temp_images.dart';
+import 'package:mobile/models/models.dart';
+
 class GameManagerService {
 // TODO : Implement GameManagerService
+
+  void testConvert() {
+    // print('Test convert called');
+    // dynamic temp = jsonDecode(messageTemps);
+    // final Differences difference = Differences.fromJson(differencesTemp);
+    List<Coordinate> coordinates = [];
+    dynamic temp = oneDifferenceTemp['differencesData']['currentDifference'];
+    for (var i = 0; i < temp.length; i++) {
+      coordinates.add(Coordinate.fromJson(temp[i]));
+    }
+
+    print(coordinates);
+    print(oneDifferenceTemp['differencesData']['currentDifference']);
+
+    // boo
+  }
 }
