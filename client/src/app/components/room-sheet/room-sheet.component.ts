2adb6aac1254e6503c4a0a45f455728e457d6f34
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-03-08 21:16:53 -0500

    refactor: refactoring

--- client/src/app/components/room-sheet/room-sheet.component.ts
+++ client/src/app/components/room-sheet/room-sheet.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Game } from '@common/game-interfaces';
@@ -9,18 +10,14 @@ import { Game } from '@common/game-interfaces';
 })
 export class RoomSheetComponent {
     @Input() game: Game;
-    mode: string = 'Mode';
-    numberOfDifferences: number = 5; // Replace with actual logic to get the number
-    players: string[] = ['Player1', 'Player2', 'player2', 'player4']; // Replace with actual logic to get player names
-    maxPlayers: number = 4;
+    gameMode: string = 'Mode';
+    numberOfDifferences: number;
+    connectedPlayers: number;
+    playerNames: string[] = [];
 
     constructor(public router: Router) {}
 
-    joinRoom() {
-        // Logic to join the room
-    }
-
     isRoomFull(): boolean {
-        return this.players.length >= this.maxPlayers;
+        return this.playerNames.length >= 4;
     }
 }
