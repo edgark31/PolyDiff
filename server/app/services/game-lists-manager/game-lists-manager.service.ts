8fe82df107f502691726d4be6568f5f4754019c3
ZakariaZair <zakaria.zair@polymtl.ca>
2024-03-12 14:58:38 -0400

    fix: game cards get

--- server/app/services/game-lists-manager/game-lists-manager.service.ts
+++ server/app/services/game-lists-manager/game-lists-manager.service.ts
@@ -21,7 +21,7 @@ export class GameListsManagerService {
             difficultyLevel: game.isHard,
             soloTopTime: DEFAULT_BEST_TIMES,
             oneVsOneTopTime: DEFAULT_BEST_TIMES,
-            thumbnail: `assets/${game.name}/original.bmp`,
+            thumbnail: `assets/${game._id.toString()}/original.bmp`,
         };
         return gameCard;
     }
