b23c4fcb2faff96f9702fb4d6f541497cf9d0587
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-03-11 20:04:30 -0400

    feat: created methods for lobby chat

--- client/src/app/pages/waiting-room/waiting-room.component.ts
+++ client/src/app/pages/waiting-room/waiting-room.component.ts
@@ -1,27 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ClientSocketService } from '@app/services/client-socket-service/client-socket.service';
 import { RoomManagerService } from '@app/services/room-manager-service/room-manager.service';
-import { Lobby } from '@common/game-interfaces';
-
+import { Chat, Lobby } from '@common/game-interfaces';
+import { Subscription } from 'rxjs';
 @Component({
     selector: 'app-waiting-room',
     templateUrl: './waiting-room.component.html',
     styleUrls: ['./waiting-room.component.scss'],
 })
-export class WaitingRoomComponent implements OnInit {
+export class WaitingRoomComponent implements OnInit, AfterViewInit, OnDestroy {
     lobby: Lobby;
-
-    constructor(public router: Router, public roomManagerService: RoomManagerService) {}
+    messages: Chat[] = [];
+    lobbySuscription: Subscription;
+    messageSubscription: Subscription;
+    constructor(public router: Router, public roomManagerService: RoomManagerService, private clientSocketService: ClientSocketService) {}
 
     ngOnInit(): void {
         this.roomManagerService.wait = true;
         this.roomManagerService.handleRoomEvents();
-        this.roomManagerService.lobby$.subscribe((lobby: Lobby) => {
+        this.lobbySuscription = this.roomManagerService.lobby$.subscribe((lobby: Lobby) => {
             this.lobby = lobby;
         });
         // this.roomManagerService.lobby$.pipe(filter((lobby) => !!lobby)).subscribe((lobby) => {
         //     this.lobby = lobby;
         //     console.log('wait' + this.lobby.players.length);
         // });
+    }
+
+    ngAfterViewInit(): void {
+        if (this.clientSocketService.isSocketAlive('lobby')) {
+            this.roomManagerService.updateLog();
+            this.messageSubscription = this.roomManagerService.message$.subscribe((message: Chat) => {
+                this.receiveMessage(message);
+            });
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.roomManagerService.wait = false;
+        this.lobbySuscription.unsubscribe();
+        this.messageSubscription.unsubscribe();
+        if (this.clientSocketService.isSocketAlive('lobby')) {
+            this.roomManagerService.off();
+        }
+    }
+
+    receiveMessage(chat: Chat): void {
+        this.messages.push(chat);
+    }
+    sendMessage(message: string): void {
+        this.roomManagerService.sendMessage(this.lobby.lobbyId, message);
     }
 }
