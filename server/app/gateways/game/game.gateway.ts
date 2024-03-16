935d0eeab6af891af4a5635bf3295d745b3ebaf1
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-03-15 22:21:30 -0400

    fix: linted server

--- server/app/gateways/game/game.gateway.ts
+++ server/app/gateways/game/game.gateway.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 /* eslint-disable object-shorthand */
 /* eslint-disable no-unused-expressions */
