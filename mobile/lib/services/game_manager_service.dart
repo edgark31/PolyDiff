762cbddb410ae0cbe0d55b410ce4483aa0aee75a
JeremyEar <jeremy_ear@hotmail.com>
2024-03-19 09:47:02 -0400

    feat: startGame and sendCoord

--- mobile/lib/services/game_manager_service.dart
+++ mobile/lib/services/game_manager_service.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mobile/constants/enums.dart';
+import 'package:mobile/models/game.dart';
 import 'package:mobile/services/socket_service.dart';
 
 class GameManagerService extends ChangeNotifier {
   final SocketService socketService = Get.find();
 
-  void startGame(String? lobbyID) {}
+  void startGame(String? lobbyId) {
+    socketService.send(SocketType.Game, GameEvents.StartGame.name);
+  }
 
-  void sendCoord() {}
+  void sendCoord(String? lobbyID, Coordinate coord) {
+    socketService.send(SocketType.Game, GameEvents.Clic.name,
+        {'lobbyId': lobbyID, 'coordClic': coord});
+  }
 }
