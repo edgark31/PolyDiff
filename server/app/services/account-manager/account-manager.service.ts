e51ccbba16de83814eafd200abcdb3d5c2ee3442
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2024-03-21 02:33:39 -0400

    fix: linted server

--- server/app/services/account-manager/account-manager.service.ts
+++ server/app/services/account-manager/account-manager.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable max-params */
 /* eslint-disable no-underscore-dangle */
+/* eslint-disable max-lines */
 import { Account, AccountDocument, Credentials, Sound, Theme } from '@app/model/database/account';
 import { ImageManagerService } from '@app/services/image-manager/image-manager.service';
 import { CORRECT_SOUND_LIST, ERROR_SOUND_LIST, THEME_PERSONALIZATION } from '@common/constants';
