6c89441697ee09180406d9619de03bcfecc485b4
JeremyEar <jeremy_ear@hotmail.com>
2024-03-22 13:02:06 -0400

    feat: added sendCoord and removeDifference handling

--- mobile/lib/services/game_manager_service.dart
+++ mobile/lib/services/game_manager_service.dart
@@ -9,15 +9,23 @@ import 'package:mobile/services/socket_service.dart';
 
 class GameManagerService extends ChangeNotifier {
   static Game _game = Game.initial();
+  static int _time = 0;
   final SocketService socketService = Get.find();
   final GameAreaService gameAreaService = Get.find();
   final LobbyService lobbyService = Get.find();
 
   Game get game => _game;
+  int get time => _time;
 
   void setGame(Game newGame) {
     print('new Game has been setted $game');
     _game = newGame;
+    notifyListeners();
+  }
+
+  void setTime(int newTime) {
+    print("New time setted");
+    _time = newTime;
     notifyListeners();
   }
 
@@ -27,6 +35,8 @@ class GameManagerService extends ChangeNotifier {
   }
 
   void sendCoord(String? lobbyID, Coordinate coord) {
+    print(
+        'SendCoord is called with id: $lobbyID and coord: x: ${coord.x} y: ${coord.y}');
     socketService.send(
       SocketType.Game,
       GameEvents.Clic.name,
@@ -40,11 +50,11 @@ class GameManagerService extends ChangeNotifier {
   void setListeners() {
     socketService.on(SocketType.Game, GameEvents.StartGame.name, (data) {
       print('StartGameReceived');
-      //final receivedData = data as Map<String, dynamic>;
       setGame(Game.fromJson(data as Map<String, dynamic>));
     });
 
     socketService.on(SocketType.Game, GameEvents.Found.name, (data) {
+      print("Difference Found");
       Map<String, dynamic> returnedInfo = data as Map<String, dynamic>;
       lobbyService.setLobby(
           Lobby.fromJson(returnedInfo['lobby'] as Map<String, dynamic>));
@@ -54,12 +64,11 @@ class GameManagerService extends ChangeNotifier {
       gameAreaService.showDifferenceFound(coord);
     });
 
-    // socketService.on(SocketType.Game, GameEvents.EndGame.name, (data) {
-    //   print(data as String?); // Server sending 'Temps écoulé !'
-    //   print('GameEvents.EndGame.name event received');
-    // });
+    socketService.on(SocketType.Game, GameEvents.TimerUpdate.name, (data) {
+      setTime(data as int);
+    });
 
-    // socketService.on(SocketType.Game, GameEvents.TimerUpdate.name, (data) {
+    // socketService.on(SocketType.Game, GameEvents.EndGame.name, (data) {
     //   print(data as String?); // Server sending 'Temps écoulé !'
     //   print('GameEvents.EndGame.name event received');
     // });
