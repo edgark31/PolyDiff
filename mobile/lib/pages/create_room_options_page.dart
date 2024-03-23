c52bfd7b6f41cd696089c504aab1bf8ae234a354
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2024-03-23 17:45:09 -0400

    feat: added setting gameBonus option for limited

--- mobile/lib/pages/create_room_options_page.dart
+++ mobile/lib/pages/create_room_options_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/constants/app_constants.dart';
 import 'package:mobile/constants/app_routes.dart';
+import 'package:mobile/constants/enums.dart';
 import 'package:mobile/services/lobby_selection_service.dart';
 import 'package:mobile/services/lobby_service.dart';
 import 'package:mobile/widgets/customs/custom_btn.dart';
@@ -25,6 +26,7 @@ class CreateRoomOptionsPage extends StatefulWidget {
 class _CreateRoomOptionsPageState extends State<CreateRoomOptionsPage> {
   bool cheatMode = false;
   double gameDuration = 30;
+  double gameBonus = 10;
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +48,15 @@ class _CreateRoomOptionsPageState extends State<CreateRoomOptionsPage> {
               padding: const EdgeInsets.only(top: 16.0),
             ),
             timeSelection(context),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+            ),
+            if (lobbyService.gameModes == GameModes.Limited) ...[
+              bonusTimeSelection(context),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+              ),
+            ],
             CustomButton(
               press: () {
                 lobbySelectionService.setIsCheatEnabled(cheatMode);
@@ -58,6 +69,7 @@ class _CreateRoomOptionsPageState extends State<CreateRoomOptionsPage> {
               },
               text: 'Créer la salle de jeu',
               backgroundColor: kMidOrange,
+              widthFactor: 0.25,
             ),
             CustomButton(
               press: () {
@@ -101,11 +113,32 @@ class _CreateRoomOptionsPageState extends State<CreateRoomOptionsPage> {
           value: gameDuration,
           min: 30,
           max: 600, // TODO: Change back to 50 when testing is done
-          divisions: 60,
+          divisions: 30,
           label: gameDuration.round().toString(),
           onChanged: (double newValue) {
             setState(() {
               gameDuration = newValue;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget bonusTimeSelection(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text('Temps de bonus pour différence trouvée (en secondes)'),
+        Slider(
+          value: gameBonus,
+          min: 10,
+          max: 20, // TODO: Change back to 50 when testing is done
+          divisions: 10,
+          label: gameBonus.round().toString(),
+          onChanged: (double newValue) {
+            setState(() {
+              gameBonus = newValue;
             });
           },
         ),
