cb2633aad324a4df2863c123655480e59f0e51d4
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-03-24 13:34:40 -0400

    fix: fixed end dialog

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -107,11 +107,11 @@ export class GamePageComponent implements OnDestroy, OnInit, AfterViewInit {
         this.timeSubscription = this.gameManager.timerLobby$.subscribe((timer: number) => {
             this.timer = timer;
         });
-        this.clientSocket.on('game', GameEvents.EndGame, () => {
-            this.showEndGameDialog(this.endMessage);
-            // this.router.navigate(['/game-mode']);
-            this.welcome.onChatGame = false;
-        });
+        // this.clientSocket.on('game', GameEvents.EndGame, () => {
+        //     this.showEndGameDialog(this.endMessage);
+        //     // this.router.navigate(['/game-mode']);
+        //     this.welcome.onChatGame = false;
+        // });
         this.endMessageSubscription = this.gameManager.endMessage$.subscribe((endMessage: string) => {
             this.endMessage = endMessage;
             this.showEndGameDialog(this.endMessage);
