1fda53383bef0fd759b8ca615f901bf6207563de
MarieJadeMarcil <marie-jade.marcil@polymtl.ca>
2024-03-26 13:21:43 -0400

    feat : added Bienvenue title to match thick client

--- mobile/lib/utils/app_theme_utils.dart
+++ mobile/lib/utils/app_theme_utils.dart
@@ -7,11 +7,9 @@ class CustomAppTheme {
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'troika',
-    scaffoldBackgroundColor: Colors.white,
     brightness: Brightness.light,
+    scaffoldBackgroundColor: Colors.white,
     primaryColor: Colors.orangeAccent.shade400,
-    colorScheme: ColorScheme.light(),
-    iconTheme: IconThemeData(color: Colors.red, opacity: 0.8),
     textTheme: CustomTextTheme.lightTextTheme,
   );
 
@@ -21,8 +19,6 @@ class CustomAppTheme {
     scaffoldBackgroundColor: Colors.grey.shade900,
     brightness: Brightness.dark,
     primaryColor: Colors.black,
-    colorScheme: ColorScheme.dark(),
-    iconTheme: IconThemeData(color: Colors.purple.shade200, opacity: 0.8),
     textTheme: CustomTextTheme.darkTextTheme,
   );
 }
--- mobile/lib/utils/text_theme.dart
+++ mobile/lib/utils/text_theme.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class CustomTextTheme {
   CustomTextTheme._();
 
-  static TextTheme lightTextTheme = TextTheme();
+  static TextTheme lightTextTheme = TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 140,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFE8A430),
+      letterSpacing: 0.0,
+    ),
+  );
   static TextTheme darkTextTheme = TextTheme();
 }
