88462d5d044e356e892fbbc72059290d1907af25
JeremyEar <jeremy_ear@hotmail.com>
2024-03-28 19:42:30 -0400

    feat: added 3 buttons that switches the page

--- mobile/lib/pages/friends_page.dart
+++ mobile/lib/pages/friends_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/constants/app_constants.dart';
 import 'package:mobile/constants/app_routes.dart';
 import 'package:mobile/widgets/customs/custom_app_bar.dart';
 import 'package:mobile/widgets/customs/custom_menu_drawer.dart';
@@ -18,16 +19,64 @@ class FriendsPage extends StatefulWidget {
 }
 
 class _FriendsPageState extends State<FriendsPage> {
+  int _selectedViewIndex = 0;
+
+  void _selectView(int index) {
+    setState(() {
+      _selectedViewIndex = index;
+    });
+  }
+
+  Widget _buildContent() {
+    switch (_selectedViewIndex) {
+      case 0:
+        return Text("Contenu pour Tous les amis");
+      case 1:
+        return Text("Contenu pour En attente");
+      case 2:
+        return Text("Contenu pour Ajouter un ami");
+      default:
+        return Text("Contenu non disponible");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: CustomMenuDrawer(),
-        appBar: CustomAppBar(title: "Page d'amis"),
-        body: Column(children: [Row( children: [
-          ElevatedButton(onPressed: () {
-            
-          }, child: Text("Tous les Amis"),)]
-        )],),
+      appBar: CustomAppBar(title: "Page d'amis"),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              for (int i = 0; i < 3; i++)
+                ElevatedButton(
+                  onPressed: () => _selectView(i),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        if (_selectedViewIndex == i) {
+                          return Color.fromARGB(255, 2, 70, 22);
+                        } else {
+                          return kLightGreen;
+                        }
+                      },
+                    ),
+                  ),
+                  child: Text(
+                      i == 0
+                          ? "Tous les amis"
+                          : (i == 1 ? "En attente" : "Ajouter un ami"),
+                      style: TextStyle(fontSize: 25, color: Colors.white)),
+                ),
+            ],
+          ),
+          Expanded(
+            child: _buildContent(),
+          ),
+        ],
+      ),
     );
   }
 }
