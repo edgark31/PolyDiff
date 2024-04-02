f95beb33f8cd6bd1ca0290e1279f17b96367edd6
JeremyEar <jeremy_ear@hotmail.com>
2024-04-02 13:36:17 -0400

    feat: added fetchUsers and handling received users needs testing

--- mobile/lib/services/friend_service.dart
+++ mobile/lib/services/friend_service.dart
@@ -1,39 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mobile/constants/enums.dart';
 import 'package:mobile/models/user_model.dart';
 import 'package:mobile/services/info_service.dart';
-import 'package:mobile/services/lobby_service.dart';
 import 'package:mobile/services/socket_service.dart';
 
 class FriendService extends ChangeNotifier {
-  // static Game _game = Game.initial();
-  static List<User> users = [];
+  static List<User> _users = [];
 
   final SocketService socketService = Get.find();
 
-  final LobbyService lobbyService = Get.find();
   final InfoService infoService = Get.find();
 
-  // Game get game => _game;
+  List<User> get users => _users;
 
-  // void updateRemainingDifferences(List<List<Coordinate>>? remaining) {
-  //   _game.differences = remaining;
-  //   notifyListeners();
-  // }
+  void updateUsersList(List<User> allUsers) {
+    _users = allUsers;
+    notifyListeners();
+  }
 
-  // void fetchUsers()
+  void fetchUsers() {
+    socketService.send(SocketType.Auth, UserEvents.UpdateUsers.name);
+  }
 
   void setListeners() {
-    // socketService.on(SocketType.Game, GameEvents.Found.name, (data) {
-    //   print("Difference Found");
-    //   Map<String, dynamic> returnedInfo = data as Map<String, dynamic>;
-    //   lobbyService.setLobby(
-    //       Lobby.fromJson(returnedInfo['lobby'] as Map<String, dynamic>));
-    //   List<Coordinate> coord = returnedInfo['difference']
-    //       .map<Coordinate>((coordinate) => Coordinate.fromJson(coordinate))
-    //       .toList();
-    //   gameAreaService.showDifferenceFound(coord);
-    // });
+    socketService.on(SocketType.Auth, UserEvents.UpdateUsers.name, (data) {
+      print("Receiving all users");
+      List<dynamic> receivedData = data as List<dynamic>;
+      List<User> allUsers =
+          receivedData.map<User>((user) => User.fromJson(user)).toList();
+      print(allUsers);
+    });
 
     // socketService.on(SocketType.Game, GameEvents.TimerUpdate.name, (data) {
     //   setTime(data as int);
