d580d2b1675bafc68fccd2d2c77fc5596d4fcd87
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2024-04-02 20:12:20 -0400

    feat: sessions and connections first version

--- mobile/lib/pages/history_page.dart
+++ mobile/lib/pages/history_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/constants/app_constants.dart';
 import 'package:mobile/constants/app_routes.dart';
+import 'package:mobile/services/info_service.dart';
 import 'package:mobile/widgets/customs/background_container.dart';
 import 'package:mobile/widgets/customs/custom_app_bar.dart';
+import 'package:provider/provider.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({super.key});
@@ -18,46 +20,59 @@ class HistoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO : Replace when logic is set up
-    final List<LoginHistory> loginHistory = [
-      LoginHistory(
-          dateTime: DateTime.now().subtract(const Duration(days: 1)),
-          action: 'Connexion'),
-      LoginHistory(
-          dateTime: DateTime.now().subtract(const Duration(days: 1, hours: 2)),
-          action: 'Déconnexion'),
-    ];
-    final List<GameHistory> gameHistory = [
-      GameHistory(
-          dateTime: DateTime.now().subtract(const Duration(days: 2)),
-          won: true),
-      GameHistory(
-          dateTime: DateTime.now().subtract(const Duration(days: 3)),
-          won: false),
-    ];
+    final infoService = context.watch<InfoService>();
+
+    final List<LoginHistory> loginHistory = infoService.connections
+        .map(
+          (connection) => LoginHistory(
+              dateTime: connection.timestamp,
+              action: connection.isConnection ? 'Connexion' : 'Déconnexion'),
+        )
+        .toList();
+
+    final List<GameHistory> gameHistory = infoService.sessions
+        .map((session) => GameHistory(
+              dateTime: session.timestamp,
+              won: session.isWinner,
+            ))
+        .toList();
 
     return Scaffold(
       appBar: CustomAppBar(title: 'H I S T O R I Q U E'),
       body: BackgroundContainer(
         backgroundImagePath: SELECTION_BACKGROUND_PATH,
-        child: ListView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             _buildSectionTitle(context, "Historique de Connexion"),
-            _buildHistorySection(loginHistory
-                .map((e) => ListTile(
-                      title: Text(e.action),
-                      subtitle: Text("${e.dateTime}"),
-                    ))
-                .toList()),
+            Expanded(
+              child: Scrollbar(
+                child: ListView(
+                  children: loginHistory
+                      .map((e) => ListTile(
+                            title: Text(e.action),
+                            subtitle: Text("${e.dateTime}"),
+                          ))
+                      .toList(),
+                ),
+              ),
+            ),
             _buildSectionTitle(context, "Historique des Parties"),
-            _buildHistorySection(gameHistory
-                .map((e) => ListTile(
-                      title: Text(e.won ? "Gagnée" : "Perdue"),
-                      subtitle: Text("${e.dateTime}"),
-                      leading: Icon(e.won ? Icons.emoji_events : Icons.close,
-                          color: e.won ? Colors.green : Colors.red),
-                    ))
-                .toList()),
+            Expanded(
+              child: Scrollbar(
+                child: ListView(
+                  children: gameHistory
+                      .map((e) => ListTile(
+                            title: Text(e.won ? "Gagnée" : "Perdue"),
+                            subtitle: Text("${e.dateTime}"),
+                            leading: Icon(
+                                e.won ? Icons.emoji_events : Icons.close,
+                                color: e.won ? Colors.green : Colors.red),
+                          ))
+                      .toList(),
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -70,27 +85,17 @@ class HistoryPage extends StatelessWidget {
       child: Text(title),
     );
   }
-
-  Widget _buildHistorySection(List<Widget> children) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: children,
-      ),
-    );
-  }
 }
 
 class LoginHistory {
-  final DateTime dateTime;
+  final String dateTime;
   final String action;
 
   LoginHistory({required this.dateTime, required this.action});
 }
 
 class GameHistory {
-  final DateTime dateTime;
+  final String dateTime;
   final bool won;
 
   GameHistory({required this.dateTime, required this.won});
