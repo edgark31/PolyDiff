d0fca73432fd8120dde87468d5afe03211c5d9a7
MarieJadeMarcil <marie-jade.marcil@polymtl.ca>
2024-04-07 11:03:32 -0400

    export all services into one file

--- mobile/lib/services/services.dart
+++ mobile/lib/services/services.dart
@@ -1,3 +1,18 @@
 // Export all of the services here
+export 'account_service.dart';
+export 'avatar_service.dart';
+export 'chat_service.dart';
+export 'coordinate_conversion_service.dart';
+export 'form_service.dart';
+export 'friend_service.dart';
+export 'game_area_service.dart';
+export 'game_card_service.dart';
+export 'game_manager_service.dart';
+export 'image_converter_service.dart';
+export 'info_service.dart';
+export 'lobby_selection_service.dart';
+export 'lobby_service.dart';
 export 'name_generation_service.dart';
+export 'register_service.dart';
 export 'socket_service.dart';
+export 'sound_service.dart';
