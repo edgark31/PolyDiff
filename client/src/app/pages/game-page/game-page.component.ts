9616cd62cd7d69ea5773d6c7a0c3fe78b81b890a
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-04-09 17:12:55 -0400

    fix: linted client

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
