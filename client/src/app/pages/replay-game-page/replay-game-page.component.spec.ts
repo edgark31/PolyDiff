b177149c3e82297f841f4f9c7408914091e96f5b
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-04-10 14:17:20 -0400

    fix: linted client

--- client/src/app/pages/replay-game-page/replay-game-page.component.spec.ts
+++ client/src/app/pages/replay-game-page/replay-game-page.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReplayGamePageComponent } from './replay-game-page.component';
 
 describe('ReplayGamePageComponent', () => {
-  let component: ReplayGamePageComponent;
-  let fixture: ComponentFixture<ReplayGamePageComponent>;
+    let component: ReplayGamePageComponent;
+    let fixture: ComponentFixture<ReplayGamePageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ReplayGamePageComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ReplayGamePageComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(ReplayGamePageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ReplayGamePageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
