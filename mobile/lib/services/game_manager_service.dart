9457113b02044d8d18c264539e7ab5728004c95c
JeremyEar <jeremy_ear@hotmail.com>
2024-04-10 14:54:01 -0400

    fix: fixed cheat mode edge case

--- mobile/lib/services/game_manager_service.dart
+++ mobile/lib/services/game_manager_service.dart
@@ -94,6 +94,7 @@ class GameManagerService extends ChangeNotifier {
 
   void abandonGame(String? lobbyId) {
     print('AbandonGame called with id: $lobbyId');
+    gameAreaService.showDifferenceFound([]);
     socketService.send(SocketType.Game, GameEvents.AbandonGame.name, lobbyId);
   }
 
@@ -190,6 +191,7 @@ class GameManagerService extends ChangeNotifier {
 
     socketService.on(SocketType.Game, GameEvents.EndGame.name, (data) {
       setEndGameMessage(data as String?);
+      gameAreaService.showDifferenceFound([]);
       disconnectSockets();
     });
 
