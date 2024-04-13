b4c2cb331e1b81faaad785d517cc39a0f1f7e825
moha0244 <mohamed-lamine.gning@polymtl.ca>
2024-04-13 18:28:52 +0000

    bug share refix

--- client/src/app/components/game-page-dialog/game-page-dialog.component.ts
+++ client/src/app/components/game-page-dialog/game-page-dialog.component.ts
@@ -75,9 +75,11 @@ export class GamePageDialogComponent {
         this.clientSocket.disconnect('lobby');
         this.clientSocket.disconnect('game');
         if (this.data.lobby.mode !== GameModes.Practice) this.goShare = true;
+        else {
+            this.dialog.closeAll();
+            this.router.navigate(['/home']);
+        }
         this.communicationService.deleteAccountId(this.replayService.record.date.toString(), this.welcomeService.account.id as string).subscribe();
-        this.dialog.closeAll();
-        this.router.navigate(['/home']);
     }
 
     replay() {
